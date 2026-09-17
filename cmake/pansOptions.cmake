80997aaba1cfc19b457b78e3d2af4cdb0c4aaa2d
changyuec <872974010@qq.com>
2026-09-17 21:32:50 +0800

    [init]初始化工程结构;创建顶层CMakeFile

---
+++ cmake/pansOptions.cmake
@@ -0,0 +1,55 @@
+add_library(pans_options INTERFACE)
+
+target_compile_options(pans_options INTERFACE
+    #GCC/Clang通用模板
+    $<$<OR:$<CXX_COMPILER_ID:GNU>,$<CXX_COMPILER_ID:Clang>>:
+        -Wall
+        -Wwxtra
+        -Wpedantic
+        -fno-strict-aliasing
+    >
+)
+
+target_compile_options(pans_options INTERFACE
+    $<$<AND:$<PLATFORM_ID:Liunx>,$<OR:$<CXX_COMPILER_ID:GNU>,$<CXX_COMPILER_ID:Clang>>>:
+        -fPIC
+    >
+)
+
+target_compile_options(pans_options INTERFACE
+    $<$<AND:$<PLATFORM_ID:Liunx>,$<OR:$<CXX_COMPILER_ID:GNU>,$<CXX_COMPILER_ID:Clang>>>:
+        -rdynamic
+    >
+)
+
+target_compile_definitions(pans_options INTERFACE
+    $<$<CONFIG:Debug>:PANS_DEBUG>
+)
+
+target_compile_options(pans_options INTERFACE 
+    $<$<CONFIG:Debug>:-O0>
+    $<$<CONFIG:Debug>:-g3>
+    $<$<CONFIG:Debug>:-ggdb>
+)
+
+# ======= Release/RelWithDebInfo =======
+target_compile_options(pans_options INTERFACE
+    $<$<CONFIG:Release>:-DNDEBUG>
+    $<$<CONFIG:Release>:-O2>
+    $<$<CONFIG:Release>:-fno-omit-frame-pointer>
+
+    $<$<CONFIG:RelWithDebInfo>:-DNDEBUG>
+    $<$<CONFIG:RelWithDebInfo>:-O2>
+    $<$<CONFIG:RelWithDebInfo>:-g>
+    $<$<CONFIG:RelWithDebInfo>:-fno-omit-frame-pointer>
+)
+
+option(ENABLE_COVERAGE "Enable code coverage instrumentation" OFF)
+if(ENABLE_COVERAGE)
+    target_compile_options(pans_options INTERFACE
+        $<$<CONFIG:Debug>:--coverage>
+    )
+    target_link_options(pans_options INTERFACE
+        $<$<CONFIG:Debug>:--coverage>
+    )
+endif()
